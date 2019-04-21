c06792597e1ddd8fdb56d8eecd3b54e5b3946e0f
VikashDrone <v.vikashsingh@gmail.com>
2019-04-21 10:40:16 +0530

    3 rd commit

--- Code_Script.R
+++ Code_Script.R
@@ -1,2 +1,11 @@
-This is the new line that should be visible
-This is the second line appended from teh local 
+# IF not installed, install the Kernlab package 
+library(kernlab)
+data(spam)
+
+# Performing the sampling step for test and trainign data set
+set.seed(3435)
+trainIndicator = rbinom(4601, size = 1, prob = 0.5)
+table(trainIndicator)
+
+trainSpam = spam [trainIndicator == 1, ]  
+testSpam  = spam [trainIndicator == 0, ] 
